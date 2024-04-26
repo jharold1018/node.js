a7bc36ae9a099b68c8dfb751bb6122c3a70a7871
jharold1018 <jharol1018@gmail.com>
2024-04-25 22:35:21 -0500

    editado desde casa

---
+++ app-paises-suramerica/src/lib/paises.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let pais;
+    let p;
+    const paises={
+        "colombia":["Colombia","Bogota","51,52 millones","Peso colombiano","1,142 millones de km²","flag_of_colombia.svg"],
+        "venezuela":["Venezuela","Caracas","28,2 millones","Bolivar","916.445 km²","flag_of_Venezuela.svg"],
+        "ecuador":["Ecuador","Quito","17,8 millones","Dolar estadounidense","256.370 km²","flag_of_Ecuador.svg"],
+        "peru":["Peru","Lima","33,72 millones","Sol peruano","1.285.215 km²","flag_of_Peru_(state).svg.webp"],
+        "brazil":["Brazil","Brasilia","214,3 millones","Real brasileño","8.51 millones km²","flag_of_Brazil.svg"]
+    }
+    p=paises[pais];
+</script>
+
+<div class="main">
+    <div class="col1">
+        <img src="./src/" alt="">
+    </div>
+</div>
